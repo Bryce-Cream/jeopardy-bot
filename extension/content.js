2ce8488ae2ac7411ae4efe46b8856e24f7c9764e
Bryce Hughson <64735781+Bryce-Cream@users.noreply.github.com>
2024-03-27 20:15:19 -0400

    Chrome is now listening for websites

--- extension/content.js
+++ extension/content.js
@@ -1,13 +1,44 @@
+// Function to send data to intermediary service
+function sendDataToIntermediaryService(data) 
+{
+  fetch('http://localhost:3000/data', 
+  {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  })
+  .then(response => 
+  {
+      if (!response.ok) {
+          throw new Error('Failed to send data to intermediary service');
+      }
+      console.log('Data sent successfully to intermediary service');
+  })
+  .catch(error => 
+  {
+      console.error('Error sending data to intermediary service:', error);
+  });
+}
+
+
 const cells = document.querySelectorAll('.cell.points');
 
 cells.forEach(cell => 
 {
-  cell.addEventListener('click', function() 
+  cell.addEventListener('click', function()
   {
     const frontAnswer = this.querySelector('.front.answer');
     if(frontAnswer.innerText.toLowerCase() === "daily double")
     {
-      alert("DAILY DOUBLE DO STUFF WE WOO");
+      const data = 
+      {
+        event: 'click',
+        dailyDouble: true
+      };
+      sendDataToIntermediaryService(data);
+      //alert("DAILY DOUBLE DO STUFF WE WOO");
     }
   });
 });
